458cd84baf933539560e9d09f4d2b07526458c75
wonsur <88186623+wonsur@users.noreply.github.com>
2025-05-16 00:43:32 +0200

    added db file

---
+++ uczniowie.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Maj 16, 2025 at 12:42 AM
+-- Wersja serwera: 10.4.28-MariaDB
+-- Wersja PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `biblioteka`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `uczniowie`
+--
+
+CREATE TABLE `uczniowie` (
+  `id_ucznia` int(11) NOT NULL,
+  `imie` varchar(255) NOT NULL,
+  `nazwisko` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `uczniowie`
+--
+
+INSERT INTO `uczniowie` (`id_ucznia`, `imie`, `nazwisko`) VALUES
+(1, '', ''),
+(2, '', ''),
+(3, 'Marek', 'Marucha');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `uczniowie`
+--
+ALTER TABLE `uczniowie`
+  ADD PRIMARY KEY (`id_ucznia`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `uczniowie`
+--
+ALTER TABLE `uczniowie`
+  MODIFY `id_ucznia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
