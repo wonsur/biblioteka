d50629137eb5513fdef6309a10ef48f528fd0203
wonsur <88186623+wonsur@users.noreply.github.com>
2025-05-16 00:37:21 +0200

    adding books to db

--- dodaj.php
+++ dodaj.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dodaj</title>
 </head>
 <body>
-    
+    <form action="dodaj.php" method="post">
+    <label for="tytul">Tytul: </label>
+    <input type="text" name="tytul">
+    <label for="autor">Autor: </label>
+    <input type="text" name="autor">
+    <input type="submit" value="Wyslij">
+    </form>
+    <?php
+        $tytul = $_POST['tytul'];
+        $autor = $_POST['autor'];
+
+        $c = mysqli_connect('localhost', 'root', '', 'biblioteka');
+
+        mysqli_query($c, "INSERT INTO `ksiazki`(`tytul`, `autor`) VALUES ('$tytul','$autor')");
+
+    ?>
+
 </body>
 </html>
