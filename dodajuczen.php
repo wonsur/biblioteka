7bc9d3d5c475f783dd01930ff1a053748f40ae67
wonsur <88186623+wonsur@users.noreply.github.com>
2025-05-16 00:40:48 +0200

    added adding uczen

--- dodajuczen.php
+++ dodajuczen.php
@@ -6,6 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-    
+<body>
+    <form action="dodajuczen.php" method="post">
+    <label for="imie">Imie: </label>
+    <input type="text" name="imie">
+    <label for="nazwisko">Nazwisko: </label>
+    <input type="text" name="nazwisko">
+    <input type="submit" value="Wyslij">
+    </form>
+    <?php
+        $imie = $_POST['imie'];
+        $nazwisko = $_POST['nazwisko'];
+
+        $c = mysqli_connect('localhost', 'root', '', 'biblioteka');
+
+        mysqli_query($c, "INSERT INTO `uczniowie`(`imie`, `nazwisko`) VALUES ('$imie','$nazwisko')");
+
+    ?>
+
+</body>
 </body>
 </html>
